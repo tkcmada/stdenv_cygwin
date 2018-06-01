7effa1f6f8ed3f0eca9100d718d294aac4affa66
Tokutaro Shimada <tokutaro@gmail.com>
2018-06-01 03:21:41 +0000

    create win32

---
+++ setup-x86.bat
@@ -0,0 +1,7 @@
+cd %~dp0
+bin\setup-x86.exe ^
+   --root %~dp0cygwin32 ^
+   --local-package-dir %CD% ^
+   --site http://ftp.iij.ad.jp/pub/cygwin/ ^
+   --quiet-mode ^
+   --packages "unzip,jq,python3,python3-devel,python3-crypto,python3-paramiko,python3-setuptools,gnupg,gcc,gcc-core,gcc-g++,git,make,patch,libncurses-devel,openssl-devel,readline,libsqlite3-devel,libxml2-devel,libxslt-devel,wget,vim,screen,Tcl,libyaml-devel,libffi-devel,openssl-devel,libmysqlclient-devel,perl,mysql"
